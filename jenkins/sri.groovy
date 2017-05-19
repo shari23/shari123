07a545b5bc4b06d44be14fe875cd18253691fe53
[srikanth] <[srikanth.hari@manheim.com]>
2017-05-19 10:34:22 -0400

    refactor groovy file

--- jenkins/sri.groovy
+++ jenkins/sri.groovy
@@ -3,17 +3,17 @@ def gitUrl = 'https://github.com/shari23/shari123.git'
 job('test job') {
 label('jenkinsslave')
 scm {
-    git {
-      remote {
-        name('origin')
-        url(gitUrl)
-    }
-      branches('master')
-      extensions {}
-    }
-    }
-    steps {
-        shell("""npm install
-              npm test""")
-    }
+git {
+remote {
+name('origin')
+url(gitUrl)
+}
+branches('master')
+extensions {}
+}
+}
+steps {
+shell("""npm install
+npm test""")
+}
 }
